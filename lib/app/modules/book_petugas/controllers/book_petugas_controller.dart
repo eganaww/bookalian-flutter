02e8162dde5fca1a2d3feb9a42f44d8f35d14df9
eganaww <geminggg81@gmail.com>
2024-04-23 22:12:30 +0700

    Get Book - Book_petugas

--- lib/app/modules/book_petugas/controllers/book_petugas_controller.dart
+++ lib/app/modules/book_petugas/controllers/book_petugas_controller.dart
@@ -1,12 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:peminjam_perpustakaan_kelas_b/app/data/constant/endpoint.dart';
+import 'package:peminjam_perpustakaan_kelas_b/app/data/model/response_book.dart';
+import 'package:peminjam_perpustakaan_kelas_b/app/data/provider/api_provider.dart';
+import 'package:peminjam_perpustakaan_kelas_b/app/data/provider/storage_provider.dart';
+import 'package:quickalert/quickalert.dart';
 
-class BookPetugasController extends GetxController {
+class BookPetugasController extends GetxController with StateMixin<List<DataBuku>>{
   //TODO: Implement BookPetugasController
+  String role = StorageProvider.read(StorageKey.Role);
 
+  final loading = false.obs;
   final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    getData();
   }
 
   @override
@@ -20,4 +30,77 @@ class BookPetugasController extends GetxController {
   }
 
   void increment() => count.value++;
+
+  Future<void> refreshData() async {
+    change(null, status: RxStatus.loading());
+    await getData();
+  }
+
+  delete(int? bukuID) async {
+    try {
+      final response = await ApiProvider.instance().delete("${Endpoint.buku}/id$bukuID");
+      QuickAlert.show(
+          context: Get.context!,
+          type: QuickAlertType.confirm,
+          title: 'Hapus Buku?',
+          confirmBtnText: 'Ya',
+          cancelBtnText: 'Tidak',
+          onConfirmBtnTap: () {
+            Get.back();
+            if (response.statusCode == 200) {
+              getData();
+              QuickAlert.show(
+                context: Get.context!,
+                type: QuickAlertType.success,
+                confirmBtnText: 'Ok',
+                title: 'Berhasil',
+                text: 'Berhasil Menghapus Koleksi',
+                titleColor: Colors.green,
+              );
+            } else {
+              QuickAlert.show(
+                context: Get.context!,
+                type: QuickAlertType.error,
+                confirmBtnText: 'Ok',
+                title: 'Gagal',
+                text: 'Gagal Menghapus Koleksi',
+                titleColor: Colors.red,
+              );
+            }
+          }
+      );
+    } catch (e) {
+      loading(false);
+      Get.snackbar("Error", e.toString(), backgroundColor: Colors.red);
+    }
+  }
+
+  Future<void> getData() async {
+    change(null, status: RxStatus.loading());
+    try {
+      final response =
+      await ApiProvider.instance().get(Endpoint.buku);
+      if (response.statusCode == 200) {
+        final ResponseBook responseBook = ResponseBook.fromJson(response.data);
+        if (responseBook.data!.isEmpty) {
+          change(null, status: RxStatus.empty());
+        } else {
+          change(responseBook.data, status: RxStatus.success());
+        }
+      } else {
+        change(null, status: RxStatus.error("Gagal mengambil data buku"));
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        if (e.response?.data != null) {
+          change(null,
+              status: RxStatus.error("${e.response?.data['message']}"));
+        }
+      } else {
+        change(null, status: RxStatus.error(e.message ?? ""));
+      }
+    } catch (e) {
+      change(null, status: RxStatus.error(e.toString()));
+    }
+  }
 }
--- lib/app/modules/book_petugas/views/book_petugas_view.dart
+++ lib/app/modules/book_petugas/views/book_petugas_view.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:peminjam_perpustakaan_kelas_b/app/routes/app_pages.dart';
+import 'package:peminjam_perpustakaan_kelas_b/app/widget/base64/base64widget.dart';
 
 import '../controllers/book_petugas_controller.dart';
 
@@ -10,15 +14,134 @@ class BookPetugasView extends GetView<BookPetugasController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BookPetugasView'),
+        title: const Text('Buku'),
         centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'BookPetugasView is working',
-          style: TextStyle(fontSize: 20),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Get.offAllNamed(Routes.DASHBOARD);
+          },
         ),
       ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await controller.refreshData();
+        },
+        child: SingleChildScrollView(
+          child: controller.obx((state) => ListView.separated(
+            shrinkWrap: true, // Agar ListView hanya memakan ruang yang diperlukan
+            physics: NeverScrollableScrollPhysics(), // Agar ListView tidak bisa di-scroll
+            itemCount: state!.length,
+            itemBuilder: (context, index){
+              return InkWell(
+                // onTap: () {
+                //   Get.toNamed(Routes.DETAIL_BOOK,
+                //       parameters: {
+                //         'id': state[index].bukuID.toString(),
+                //         'judul': state[index].judul ?? ' ',
+                //       }
+                //   );
+                // },
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Card(
+                    child: Container(
+                      height: 250,
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white38,
+                        border: Border.all(
+                          color: Colors.transparent,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(12),
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          SizedBox(width: 20),
+                          Container(
+                            width: 100,
+                            height: 150,
+                            child: FittedBox(
+                              fit: BoxFit.fill,
+                              child: state[index].cover == 'NONE'
+                                  ? Image.asset('assets/img/not-found.png')  // Ganti dengan gambar not-found.png
+                                  : Image(image: base64widget(state[index].cover!))
+                            ), // Ubah dari base64 menjadi Image.memory
+                          ),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 20),
+                                Text(
+                                  "${state[index].judul}",
+                                ),
+                                Text(
+                                  "Pengarang  : ${state[index].penulis}",
+                                ),
+                                Text(
+                                  "Penerbit   : ${state[index].penerbit}",
+                                ),
+                                SizedBox(height: 40),
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          controller.delete(state[index].bukuID).then((value) {
+                                            if (value) {
+                                              // Refresh the list after successful deletion
+                                              controller.getData();
+                                            }
+                                          });
+                                        },
+                                        child: Icon(
+                                          Icons.delete
+                                        ),
+                                        style: ButtonStyle(
+                                          backgroundColor: MaterialStateProperty.all<Color>(Colors.red), // Ubah warna latar belakang
+                                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // Ubah warna teks
+                                          minimumSize: MaterialStateProperty.all<Size>(Size(50.0, 40.0)), // Mengatur tinggi minimum
+                                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                            RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(15.0),
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return Divider();
+            },
+          )),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //
+        },
+        backgroundColor: Colors.green.shade900, // Warna background merah
+        child: Icon(Icons.add, color: Colors.white,)
+    ),
     );
+
   }
 }
